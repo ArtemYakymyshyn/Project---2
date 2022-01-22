0be91949ece72df074de97f1060eb6db062d8014
Sepp <95585848+ArtemYakynyshyn@users.noreply.github.com>
2022-01-22 15:26:50 +0200

    Add files via upload

---
+++ Shades/js/script.js
@@ -0,0 +1,6 @@
+$('.fs-burger-content').on('click', function (e) {
+   e.preventDefault;
+   $('.fs-burger-content').toggleClass('burger-content_active')
+   $('.fs-ele-1-list-button').toggleClass('fs-ele-1-list-button_active')
+
+});
